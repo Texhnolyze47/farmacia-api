ed5f6db4f65af0bb15ddbf3ae9eb50f8b4d2e8cb
Texhnolyze47 <ichigobankai136@gmail.com>
2023-03-18 13:14:44 -0600

    add: endpoint id y renombre de endpoint

--- src/main/java/com/texhnolyze/farmacia/controllers/ControllerProduct.java
+++ src/main/java/com/texhnolyze/farmacia/controllers/ControllerProduct.java
@@ -1,6 +1,7 @@
 package com.texhnolyze.farmacia.controllers;
 
 import com.texhnolyze.farmacia.entities.Product;
+import com.texhnolyze.farmacia.service.ProductService;
 import com.texhnolyze.farmacia.service.ProductServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,22 +13,27 @@ import java.util.List;
 @RequestMapping("/products")
 public class ControllerProduct {
 
-    private ProductServiceImpl productService;
+    private ProductService productService;
 
-    public ControllerProduct(ProductServiceImpl productService) {
+    public ControllerProduct(ProductService productService) {
         this.productService = productService;
     }
 
     @GetMapping
-    public ResponseEntity<List<Product>> getAll(){
+    public ResponseEntity<List<Product>> getAllProduct(){
         List<Product> allProducts = productService.getAllProducts();
         return ResponseEntity.ok(allProducts);
     }
 
     @PostMapping
-    public ResponseEntity<Product> save(@RequestBody Product product){
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product){
         Product newProduct = productService.saveProduct(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(newProduct);
+    }
 
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> getProduct(@PathVariable String productId){
+        return null;
     }
 }
