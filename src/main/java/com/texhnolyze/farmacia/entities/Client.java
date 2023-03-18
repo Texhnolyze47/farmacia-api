03a184c2d108b5243e4913bb056502b5d14e37e0
Texhnolyze47 <ichigobankai136@gmail.com>
2023-03-18 15:50:11 -0600

    add: employee entity

---
+++ src/main/java/com/texhnolyze/farmacia/entities/Client.java
@@ -0,0 +1,23 @@
+package com.texhnolyze.farmacia.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String number;
+}
