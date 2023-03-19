70e437d883caa738108c1be4f4c723e79e8c800d
Texhnolyze47 <ichigobankai136@gmail.com>
2023-03-18 18:05:47 -0600

    add: termino la implementacion de los endpoints  de Employee

--- src/main/java/com/texhnolyze/farmacia/repositories/EmployeeRepository.java
+++ src/main/java/com/texhnolyze/farmacia/repositories/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.texhnolyze.farmacia.repositories;
 
 import com.texhnolyze.farmacia.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
---
+++ src/main/java/com/texhnolyze/farmacia/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.texhnolyze.farmacia.service;
+
+import com.texhnolyze.farmacia.entities.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    Employee saveEmployee(Employee employee);
+
+    Employee getEmployee(Long employeeId);
+
+    List<Employee> getAllEmployee();
+}
