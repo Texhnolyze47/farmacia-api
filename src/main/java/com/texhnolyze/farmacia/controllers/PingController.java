de09e706d268dd0fe41602ba739029b9c6b343cd
Texhnolyze47 <ichigobankai136@gmail.com>
2023-03-18 22:25:01 -0600

    add: se agrego un controlador para comprobar la si la api sigue en linea

---
+++ src/main/java/com/texhnolyze/farmacia/controllers/PingController.java
@@ -0,0 +1,12 @@
+package com.texhnolyze.farmacia.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
+}
