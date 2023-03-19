6afd3032903bea4e0ab5da5383f1b5108b03c375
Texhnolyze47 <ichigobankai136@gmail.com>
2023-03-18 18:06:51 -0600

    add: se termino la implemementacion de los endpoints de ControllerEmployee

--- src/main/java/com/texhnolyze/farmacia/controllers/ControllerEmployee.java
+++ src/main/java/com/texhnolyze/farmacia/controllers/ControllerEmployee.java
@@ -1,16 +1,37 @@
 package com.texhnolyze.farmacia.controllers;
 
 import com.texhnolyze.farmacia.entities.Employee;
+import com.texhnolyze.farmacia.entities.Sale;
+import com.texhnolyze.farmacia.service.EmployeeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class ControllerEmployee {
-    @PostMapping
-    public ResponseEntity<Employee> addEmployee(){
 
+    private EmployeeService employeeService;
+
+    public ControllerEmployee(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee newEmployee = employeeService.saveEmployee(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newEmployee);
+    }
+
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        List<Employee> allEmployees = employeeService.getAllEmployee();
+        return ResponseEntity.ok(allEmployees);
+    }
+
+    public ResponseEntity<Employee> getEmployee(@PathVariable Long EmployeeId){
+        Employee idEmployee = employeeService.getEmployee(EmployeeId);
+        return ResponseEntity.ok(idEmployee);
     }
 }
