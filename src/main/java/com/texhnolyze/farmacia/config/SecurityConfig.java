eb64ff6c19b055965083d6f6d1865a78a28e2617
Texhnolyze47 <ichigobankai136@gmail.com>
2023-03-20 10:53:54 -0600

    spring can use the filter

--- src/main/java/com/texhnolyze/farmacia/config/SecurityConfig.java
+++ src/main/java/com/texhnolyze/farmacia/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.texhnolyze.farmacia.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final Filter jwtAuthFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
@@ -14,7 +21,8 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                // Spring use the filter before authenticate the user
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
 
     }
