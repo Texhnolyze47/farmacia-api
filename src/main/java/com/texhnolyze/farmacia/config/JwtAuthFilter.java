be38d4d2ad09fe4629ea970b970076b4e14c89c0
Texhnolyze47 <ichigobankai136@gmail.com>
2023-03-20 10:48:27 -0600

    add: dependency for jwtUtilis

---
+++ src/main/java/com/texhnolyze/farmacia/config/JwtAuthFilter.java
@@ -0,0 +1,6 @@
+package com.texhnolyze.farmacia.config;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtAthFilter extends OncePerRequestFilter {
+}
---
+++ src/main/java/com/texhnolyze/farmacia/config/JwtUtils.java
@@ -0,0 +1,2 @@
+package com.texhnolyze.farmacia.config;public class JwtUtils {
+}
