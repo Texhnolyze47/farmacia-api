11a93bf60ce48b718d203cb33d2812b8d5bfe49e
Texhnolyze47 <ichigobankai136@gmail.com>
2023-03-24 08:52:41 -0600

    Logging en AuthenticationController

--- src/main/java/com/texhnolyze/farmacia/controllers/AuthenticationController.java
+++ src/main/java/com/texhnolyze/farmacia/controllers/AuthenticationController.java
@@ -4,6 +4,8 @@ import com.texhnolyze.farmacia.config.JwtUtils;
 import com.texhnolyze.farmacia.dao.UserDao;
 import com.texhnolyze.farmacia.dto.AuthenticationRequest;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,8 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
-    private  final AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
     private final UserDao userDao;
+
+    private Logger log = LoggerFactory.getLogger(AuthenticationController.class);
     private final JwtUtils jwtUtils;
     @PostMapping("/authenticate")
     public ResponseEntity<String> authenticate(
@@ -27,11 +31,12 @@ public class AuthenticationController {
                 authenticationRequest.getEmail(),
                 authenticationRequest.getPassword())
         );
+        log.trace("El usuario con el correo " + authenticationRequest.getEmail());
         final UserDetails user = userDao.findUserByEmail(authenticationRequest.getEmail());
         if (user != null){
             return ResponseEntity.ok(jwtUtils.generateToken(user));
         }
+        log.trace("El token que se genero es: " + jwtUtils.generateToken(user));
         return ResponseEntity.status(400).body("Some error has occurred");
-
     }
 }
