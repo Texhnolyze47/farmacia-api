c751fdac41d7c6b6d97b90726bfdb1188350fab4
Texhnolyze47 <ichigobankai136@gmail.com>
2023-05-27 22:06:32 -0600

    Se crearon las tablas de la base de datos

---
+++ src/main/java/com/texhnolyze/farmacia/entities/Client.java
@@ -0,0 +1,26 @@
+package com.texhnolyze.farmacia.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "clients")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "number")
+    private String number;
+
+}
