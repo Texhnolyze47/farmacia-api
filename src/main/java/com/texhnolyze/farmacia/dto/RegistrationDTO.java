970e3dbfdabba9728c7c3e4a4290b4ae7e378a9b
Texhnolyze47 <ichigobankai136@gmail.com>
2023-05-28 11:04:18 -0600

    add: Se agregaron validaciones a RegistrationDTO.java

--- src/main/java/com/texhnolyze/farmacia/dto/RegistrationDTO.java
+++ src/main/java/com/texhnolyze/farmacia/dto/RegistrationDTO.java
@@ -1,3 +1,18 @@
 package com.texhnolyze.farmacia.dto;
 
-public record RegistrationDTO(String username, String password) {}
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record RegistrationDTO(
+        @NotNull(message = "Username cannot be null")
+        @NotBlank(message = "Username cannot be blank")
+        @Size(min = 5, max = 20, message = "Username must be between {min} and {max} characters")
+        String username,
+        @NotNull(message = "Password cannot be null")
+        @NotBlank(message = "Password cannot be blank")
+        @Size(min = 8, max = 20, message = "Password must be between {min} and {max} characters")
+        String password
+) {
+}
