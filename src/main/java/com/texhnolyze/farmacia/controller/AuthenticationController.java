13e3778c2a26d6651c7a668cdd59756c08e2f871
Texhnolyze47 <ichigobankai136@gmail.com>
2023-05-28 00:40:44 -0600

    add: logging y se evita regresar la info del user de forma inncesaria

--- src/main/java/com/texhnolyze/farmacia/controller/AuthenticationController.java
+++ src/main/java/com/texhnolyze/farmacia/controller/AuthenticationController.java
@@ -5,13 +5,14 @@ import com.texhnolyze.farmacia.entities.User;
 import com.texhnolyze.farmacia.service.AuthenticationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin("*")
 public class AuthenticationController {
-    private static final Logger logger = LoggerFactory.getLogger(AuthenticationService.class);
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
     private final AuthenticationService authenticationService;
 
     public AuthenticationController(AuthenticationService authenticationService) {
@@ -19,10 +20,11 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public User registerUser(@RequestBody RegistrationDTO registrationDTO) {
+    public ResponseEntity<String> registerUser(@RequestBody RegistrationDTO registrationDTO) {
         logger.info("AuthenticationController - registerUser");
         logger.info("info: {}", registrationDTO);
-        return authenticationService.registerUser(registrationDTO.username(), registrationDTO.password());
+        authenticationService.registerUser(registrationDTO.username(), registrationDTO.password());
+        return ResponseEntity.ok("User registered successfully");
     }
 
 }
