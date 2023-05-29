59677dc3e4f20b0938d11bc2584daf0c90ad1b34
Texhnolyze47 <ichigobankai136@gmail.com>
2023-05-28 20:48:38 -0600

    add: se agrego el endpoint para login que regresa un jwt

--- src/main/java/com/texhnolyze/farmacia/controller/AuthenticationController.java
+++ src/main/java/com/texhnolyze/farmacia/controller/AuthenticationController.java
@@ -1,7 +1,10 @@
 package com.texhnolyze.farmacia.controller;
 
-import com.texhnolyze.farmacia.dto.RegistrationDTO;
+import com.texhnolyze.farmacia.dto.LoginRequestDTO;
+import com.texhnolyze.farmacia.dto.LoginResponseDTO;
+import com.texhnolyze.farmacia.dto.RegistrationRequestDTO;
 import com.texhnolyze.farmacia.exceptions.UsernameAlreadyTakenException;
+import com.texhnolyze.farmacia.exceptions.UsernameNotFoundException;
 import com.texhnolyze.farmacia.service.AuthenticationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +14,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @CrossOrigin("*")
 public class AuthenticationController {
     private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
@@ -23,16 +26,26 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@Validated @RequestBody RegistrationDTO registrationDTO) {
+    public ResponseEntity<String> registerUser(@Validated @RequestBody RegistrationRequestDTO registrationDTO) {
         logger.info("AuthenticationController - registerUser");
         try {
             authenticationService.registerUser(registrationDTO.username(), registrationDTO.password());
             logger.trace("info: {}", registrationDTO);
             return ResponseEntity.ok("User registered successfully");
         }catch (UsernameAlreadyTakenException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            throw new UsernameAlreadyTakenException(e.getMessage(), HttpStatus.CONFLICT);
         }
+    }
 
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> loginUser (@Validated @RequestBody LoginRequestDTO login) {
+        try {
+            LoginResponseDTO user = authenticationService.loginUser(login.username(), login.password());
+            logger.trace("user info: {}", user);
+            return ResponseEntity.ok(user);
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
 }
--- src/main/java/com/texhnolyze/farmacia/service/AuthenticationService.java
+++ src/main/java/com/texhnolyze/farmacia/service/AuthenticationService.java
@@ -1,5 +1,6 @@
 package com.texhnolyze.farmacia.service;
 
+import com.texhnolyze.farmacia.dto.LoginResponseDTO;
 import com.texhnolyze.farmacia.entities.Role;
 import com.texhnolyze.farmacia.entities.User;
 import com.texhnolyze.farmacia.exceptions.UsernameAlreadyTakenException;
@@ -8,6 +9,11 @@ import com.texhnolyze.farmacia.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -21,17 +27,21 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
-    public AuthenticationService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    public AuthenticationService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, TokenService tokenService) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
     }
 
     public User registerUser(String username,String password) {
         logger.info("in Authentication Service - register");
         if (isUsersExists(username)) {
-            throw new UsernameAlreadyTakenException("Username already taken");
+            throw new UsernameAlreadyTakenException("Username already taken", HttpStatus.CONFLICT);
         }
         String encodedPassword = passwordEncoder.encode(password);
         Role userRole = roleRepository.findByAuthority("USER").orElseThrow(() -> new RuntimeException("USER role not found"));
@@ -40,6 +50,19 @@ public class AuthenticationService {
         Set<Role> authorities = new HashSet<>();
         authorities.add(userRole);
         return userRepository.save(new User(0,username,encodedPassword,authorities));
+
+    }
+
+    public LoginResponseDTO loginUser(String username, String password) {
+            logger.info("login user: {} -- {}", username, password );
+            if (!isUsersExists(username)) {
+                throw new UsernameNotFoundException("User not found");
+            }
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
+            String token = tokenService.generateJwt(auth);
+            return new LoginResponseDTO(username,token);
     }
 
     private boolean isUsersExists(String username) {
