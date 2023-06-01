50bf4918cdcd2100f0308a063a9076ebef5cf4ef
Texhnolyze47 <ichigobankai136@gmail.com>
2023-05-31 23:50:43 -0600

    Ahora el admin puede cambiar los roles de los demas

--- src/main/java/com/texhnolyze/farmacia/controller/AdminController.java
+++ src/main/java/com/texhnolyze/farmacia/controller/AdminController.java
@@ -1,15 +1,42 @@
 package com.texhnolyze.farmacia.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.texhnolyze.farmacia.entities.Role;
+import com.texhnolyze.farmacia.entities.User;
+import com.texhnolyze.farmacia.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/admin")
 public class AdminController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
     public String getlevel() {
         return "Admin level";
+    }
+
+    @PutMapping("/{username}/role")
+    public ResponseEntity<String> changeRole(@PathVariable String username,@RequestBody Role role) {
+        Optional<User> existUser = userService.findUserByUsername(username);
+        if (existUser.isPresent()) {
+            User user = existUser.get();
+            logger.info("User - set  {}", user);
+            userService.saveUser(user,role);
+            logger.info("user - final {}", user);
+            return ResponseEntity.ok("Changed role");
+        }
+        return ResponseEntity.badRequest().body("failed to update role");
     }
 }
--- src/main/java/com/texhnolyze/farmacia/service/UserService.java
+++ src/main/java/com/texhnolyze/farmacia/service/UserService.java
@@ -1,5 +1,7 @@
 package com.texhnolyze.farmacia.service;
 
+import com.texhnolyze.farmacia.entities.Role;
+import com.texhnolyze.farmacia.entities.User;
 import com.texhnolyze.farmacia.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +9,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -23,6 +29,16 @@ public class UserService implements UserDetailsService {
         logger.info("in the user details service");
 
         return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 
+    public Optional<User> findUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+    public void saveUser(User user,Role role) {
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        logger.info(String.format("Saving user with username %s and de role %s", user.getUsername(), role.getAuthority()));
+        userRepository.save(new User(user.getUserId(), user.getUsername(), user.getPassword(), roles));
+        logger.info("User with username {} saved successfully", user.getUsername());
     }
 }
