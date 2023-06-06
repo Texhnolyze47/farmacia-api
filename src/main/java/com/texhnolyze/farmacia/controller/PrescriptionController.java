d0240b0f1be5c28a055fbba88ab3a508ef9c0c0b
Texhnolyze47 <ichigobankai136@gmail.com>
2023-06-06 09:44:19 -0600

    Cambio de url

--- src/main/java/com/texhnolyze/farmacia/controller/PrescriptionController.java
+++ src/main/java/com/texhnolyze/farmacia/controller/PrescriptionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/prescription")
+@RequestMapping("/api/prescription")
 public class PrescriptionController {
     private final PrescriptionService prescriptionService;
 
