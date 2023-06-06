c37eca7fb37788f9abd0549fb65076958d7fea7a
Texhnolyze47 <ichigobankai136@gmail.com>
2023-06-06 09:48:47 -0600

    add: name to RegistrationRequestDTO

--- src/main/java/com/texhnolyze/farmacia/dto/RegistrationRequestDTO.java
+++ src/main/java/com/texhnolyze/farmacia/dto/RegistrationRequestDTO.java
@@ -6,6 +6,10 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record RegistrationRequestDTO(
+        @NotNull(message = "name cannot be null")
+        @NotBlank(message = "name cannot be blank")
+        @Size(min = 5, max = 20, message = "name must be between {min} and {max} characters")
+        String name,
         @NotNull(message = "Username cannot be null")
         @NotBlank(message = "Username cannot be blank")
         @Size(min = 5, max = 20, message = "Username must be between {min} and {max} characters")
