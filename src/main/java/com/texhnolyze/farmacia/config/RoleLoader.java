684c22612dbed7786f78e0468473819016016c88
Texhnolyze47 <ichigobankai136@gmail.com>
2023-06-11 13:35:04 -0600

    add: componente para los roles de la db

--- src/main/java/com/texhnolyze/farmacia/config/RoleLoader.java
+++ src/main/java/com/texhnolyze/farmacia/config/RoleLoader.java
@@ -1,2 +1,27 @@
-package com.texhnolyze.farmacia.config;public class RoleLoader {
+package com.texhnolyze.farmacia.config;
+
+import com.texhnolyze.farmacia.entities.Role;
+import com.texhnolyze.farmacia.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class RoleLoader implements CommandLineRunner {
+    private final RoleRepository repository;
+
+    public RoleLoader(RoleRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role adminRole = new Role("ADMIN");
+        Role userRole = new Role("USER");
+        Role doctorRole = new Role("DOCTOR");
+        Role supportRole = new Role("SUPPORT");
+        Role sellerRole = new Role("SELLER");
+        repository.saveAll(Arrays.asList(adminRole, userRole,doctorRole,supportRole,sellerRole));
+    }
 }
